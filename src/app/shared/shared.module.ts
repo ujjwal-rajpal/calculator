43511bf6bd3b0ecdeb09a33af463da5886dcebda
Ujjwal Rajpal <ujjwalr@mindfiresolutions.com>
2021-01-04 16:33:02 +0530

    added flex layout and material module dependencies

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MyMaterialModule } from './material.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    MyMaterialModule 
+  ],exports: [
+    FlexLayoutModule,
+    MyMaterialModule
+  ]
+})
+export class SharedModule { }
