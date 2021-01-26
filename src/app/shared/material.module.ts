af3231cd7c068567575c70899a589b1c9b3cb3ac
Ujjwal Rajpal <ujjwalr@mindfiresolutions.com>
2021-01-26 22:08:34 +0530

    Create and place buttons

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 
+/**material components */
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+
 @NgModule({
     imports:[
-       
+        MatButtonModule,
+        MatIconModule,
+        MatTableModule
     ],
     exports:[
-       
+        MatButtonModule,
+        MatIconModule,
+        MatTableModule
     ]
 })
 
