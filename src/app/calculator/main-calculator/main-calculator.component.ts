d86657035919a276d2ecb161f374bc5a00eb2b0f
Ujjwal Rajpal <ujjwalr@mindfiresolutions.com>
2021-01-26 23:48:45 +0530

    add buttons functionality

--- src/app/calculator/main-calculator/main-calculator.component.ts
+++ src/app/calculator/main-calculator/main-calculator.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-calculator.component.scss']
 })
 export class MainCalculatorComponent implements OnInit {
-
+  equation:string ='';
+  result:number;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  /**
+   * used to insert values 
+   * @param value 
+   */
+  insertValue(value){
+    this.equation = this.equation+value;
+    let res = eval(this.equation);
+    if(res != this.equation){
+      this.result = res;
+    }
+  }
+  /**
+   * used to insert operator
+   * @param value 
+   */
+  insertOperator(value){
+    this.equation = this.equation+value;
+    this.result= null;
+  }
+  /**
+   * fuctionality of cancel button
+   */
+  cancelOperator(){
+    this.equation = '';
+    this.result = null;
+  }
 }
